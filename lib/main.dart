77b1e6e9c3b5ec4fbec5a8383047952d5781e44d
Hillary Kiprono <kipronohillary240@gmail.com>
2023-02-16 15:36:32 +0300

    btnclicked

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  buttonPressed(String buttonText){}
+  String equation = "0";
+  String result = "0";
+  String expression = "";
+  double equationFontSize = 38.0;
+  double resultFontSize = 48.0;
+  buttonPressed(String buttonText){
+    setState(() {
+      if(buttonText == "C"){
+        // equation = "0";
+        // result = "0";
+        // equationFontSize = 38.0;
+        // resultFontSize = 48.0;
+      }
+      else if(buttonText == "⌫"){
+        // equationFontSize = 48.0;
+        // resultFontSize = 38.0;
+        // equation = equation.substring(0, equation.length - 1);
+        // if(equation == ""){
+        //   equation = "0";
+        }
+
+      else if(buttonText=="="){
+        // equationFontSize=48.0;
+        // resultFontSize = 38.0;
+        // resultFontSize = 48.0;
+        //
+        // expression = equation;
+        // expression = expression.replaceAll('×', '*');
+        // expression = expression.replaceAll('÷', '/');
+
+      }
+
+
+      else
+      {
+        if(equation=="0"){
+          equation=buttonText;
+        }
+        else{
+          equation=equation+buttonText;
+        }
+
+
+      }
+    }
+
+    );
+  }
 
   Widget buildButton(String buttonText, double buttonHeight, Color buttonColor){
     return Container(
@@ -45,7 +92,9 @@ class _HomePageState extends State<HomePage> {
                                         )
                                     )
                                 ),
-                                onPressed: () {  },
+                                onPressed: () {
+                                  buttonPressed(buttonText);
+                                },
                                 child:  Text(
                                     buttonText,
                                   style: TextStyle(
@@ -70,15 +119,15 @@ class _HomePageState extends State<HomePage> {
 
           Container(
             alignment: Alignment.centerRight,
-            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-           // child: Text(equation, style: TextStyle(fontSize: equationFontSize),),
+            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+           child: Text(equation, style: TextStyle(fontSize: equationFontSize),),
           ),
 
 
           Container(
             alignment: Alignment.centerRight,
-            padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
-           // child: Text(result, style: TextStyle(fontSize: resultFontSize),),
+            padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+            child: Text(result, style: TextStyle(fontSize: resultFontSize),),
           ),
 
 
